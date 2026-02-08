5afcb8e9f6e31a707949edd000f8da6d2f6006e6
igormdrsr <igor7.mdrs@gmail.com>
2026-02-08 15:26:39 -0300

    change some styles on tast list screen

--- my_project/core/forms.py
+++ my_project/core/forms.py
@@ -1,7 +1,29 @@
 from django import forms
 from .models import Task
 
+
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ["title", "description", "category"]
+        # widgets = {
+        #     "title": forms.TextInput(
+        #         attrs={
+        #             "class": "w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500",
+        #             "placeholder": "Ex: Comprar ingredientes",
+        #         }
+        #     ),
+        #     "description": forms.Textarea(
+        #         attrs={
+        #             "class": "w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500",
+        #             "placeholder": "Descreva a tarefa...",
+        #             "rows": 3,
+        #         }
+        #     ),
+        #     "category": forms.TextInput(
+        #         attrs={
+        #             "class": "w-full rounded-md border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500",
+        #             "placeholder": "Ex: Trabalho, Pessoal...",
+        #         }
+        #     ),
+        # }
